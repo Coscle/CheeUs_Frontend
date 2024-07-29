4c5770d4a49ec5234f6b8bc0e4a1251195228ac1
sys <ssys0729@gmail.com>
2024-07-29 17:20:59 +0900

    frontend 갱신 전 저장

--- src/store/BoardSlice.js
+++ src/store/BoardSlice.js
@@ -8,145 +8,6 @@ const initialState = {
   searchQuery: '',
   likedMap: {},
 };
-/*
-  boards: [
-    {
-      id: 1,
-      author_id: 'rbfl8484@gmail.com',
-      author_name: "안녕",
-      category: 1,
-      title: "첫 번째 게시물",
-      content: "첫 번째 게시물 내용입니다.agdagadgadgadsdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddagdadagaeadgadgadgadgadgadgadgadgadgadgadgadadgadgadagd",
-      writeday: "2024-07-11",
-      views: 50,
-      like: 10,
-      repl_cnt: 3,
-      photoes: "https://www.w3schools.com/w3images/mac.jpg",
-    },
-    {
-      id: 2,
-      author_id: 'abc',
-      author_name: "Jane Smith",
-      category: 1,
-      title: "두 번째 게시물",
-      content: "두 번째 게시물 내용입니다.",
-      writeday: "2024-07-10",
-      views: 30,
-      like: 5,
-      repl_cnt: 2,
-      photoes: "",
-    },
-    {
-      id: 3,
-      author_id: 103,
-      author_name: "Michael Johnson",
-      category: 1,
-      title: "세 번째 게시물",
-      content: "세 번째 게시물 내용입니다.",
-      writeday: "2024-07-09",
-      views: 20,
-      like: 8,
-      repl_cnt: 1,
-      photoes: "",
-    },
-    {
-      id: 4,
-      author_id: 104,
-      author_name: "Emily Brown",
-      category: 1,
-      title: "네 번째 게시물",
-      content: "네 번째 게시물 내용입니다.",
-      writeday: "2024-07-08",
-      views: 25,
-      like: 6,
-      repl_cnt: 2,
-      photoes: "https://www.w3schools.com/html/frenchfood.jpg",
-    },
-    {
-      id: 5,
-      author_id: 'rbfl8484@gmail.com',
-      author_name: '안녕',
-      category: 2,
-      title: '첫 번째 비디오 게시물',
-      content: '첫 번째 비디오 게시물 어쩌고 저쩌고',
-      writeday: '2024-07-11',
-      views: 50,
-      like: 10,
-      repl_cnt: 3,
-      media: 'V',
-      videoUrl: 'https://www.w3schools.com/html/mov_bbb.mp4',
-    },
-    {
-      id: 6,
-      author_id: 102,
-      author_name: 'Jane Smith',
-      category: 2,
-      title: '두 번째 비디오 게시물',
-      content: '두 번째 비디오 게시물',
-      writeday: '2024-07-10',
-      views: 30,
-      like: 5,
-      repl_cnt: 2,
-      media: 'V',
-      videoUrl: 'https://www.w3schools.com/html/movie.mp4',
-    },
-    {
-      id: 7,
-      author_id: 103,
-      author_name: 'Michael Johnson',
-      category: 2,
-      title: '세 번째 비디오 게시물',
-      content: '세 번째 비디오 게시물',
-      writeday: '2024-07-09',
-      views: 20,
-      like: 8,
-      repl_cnt: 1,
-      media: 'V',
-      videoUrl: 'https://assets.codepen.io/6093409/river.mp4',
-    },
-    {
-      id: 8,
-      author_id: 'rbfl8484@gmail.com',
-      author_name: "안녕",
-      category: 3,
-      title: "맥주 빨리마시기 대회",
-      content: "맥주를 빨리마시면 숙취를 얻습니다.",
-      writeday: "2024-07-09",
-      views: 20,
-      like: 8,
-      repl_cnt: 1,
-      photoes: "",
-    },
-    {
-      id: 9,
-      author_id: 105,
-      author_name: "빙그레",
-      category: 3,
-      title: "바나나 막걸리 먹으면",
-      content: "나한테 바나나?그래서 말걸리?",
-      writeday: "2024-07-09",
-      views: 20,
-      like: 8,
-      repl_cnt: 1,
-      photoes: "",
-    },
-  ],
-  filteredBoards: [], // 필터링된 게시물 목록
-  searchQuery: '', // 검색어 상태
-  likedMap: {},
-};
- */
-
-// 게시물 목록을 가져오는 thunk
-/*
-export const fetchBoards = createAsyncThunk(
-  'board/fetchBoards',
-  async () => {
-    const response = await axios.get('http://localhost:8080/board');
-    return response.data;
-  }
-);
-*/
 
 // 카테고리에 따른 게시물 목록을 가져오는 thunk
 export const fetchBoards = createAsyncThunk(
@@ -169,13 +30,15 @@ export const addBoard = createAsyncThunk(
     const formData = new FormData();
     formData.append('title', boardData.title);
     formData.append('content', boardData.content);
-    formData.append('videoFile', boardData.videoFile);
+    /*
+    if (boardData.media instanceof File) {
+        formData.append('media', boardData.media);
+    } else {
+        console.error("Media is not a valid file");
+    }
+     */
 
-    const response = await axios.post('http://localhost:8080/board', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    });
+    const response = await axios.post('http://localhost:8080/board/insert', formData);
     return response.data;
   }
 );
